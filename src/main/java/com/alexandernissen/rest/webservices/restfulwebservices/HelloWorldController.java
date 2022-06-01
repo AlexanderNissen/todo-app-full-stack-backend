a4650428ede8b24274d7b72aad13bba202beb57c
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-06-01 21:55:40 +0200

    S5S30 - Add route with path variable to HelloWorld service

--- src/main/java/com/alexandernissen/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/alexandernissen/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.alexandernissen.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloWorldController {
@@ -16,5 +17,10 @@ public class HelloWorldController {
   @GetMapping(path = "/hello-world-bean")
   public HelloWorldBean HelloWorldBean() {
     return new HelloWorldBean("Hello, World!");
+  }
+
+  @GetMapping(path = "hello-world/path-variable/{name}")
+  public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+    return new HelloWorldBean(String.format("Hello, %s", name));
   }
 }
