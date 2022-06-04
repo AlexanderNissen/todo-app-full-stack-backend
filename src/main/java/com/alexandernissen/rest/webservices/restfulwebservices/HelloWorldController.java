2724cf9a44757dfceb13fdd82a840fb91800b3ee
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-06-04 23:08:10 +0200

    S6S35 - Enable cross-origin HTTP requests (between backend and frontend)

--- src/main/java/com/alexandernissen/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/alexandernissen/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.alexandernissen.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class HelloWorldController {
   String message;
 
