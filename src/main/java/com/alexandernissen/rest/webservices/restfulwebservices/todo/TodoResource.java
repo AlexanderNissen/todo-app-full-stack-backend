7c4048ed1d3cd0a766a34280b80b7170818a5218
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-06-14 22:10:48 +0200

    S6S42 - Connecting React front end with Todo list from REST service

--- src/main/java/com/alexandernissen/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/alexandernissen/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class TodoResource {
 
   @Autowired
