1fe20f8d7b52a6511e737e026b30284b5804432e
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-09-05 21:49:57 +0200

    Configure Spring Boot basic auth. Edit pom to older version of Spring Security to make WebSecurityConfigurerAdapter available for extension

---
+++ src/main/java/com/alexandernissen/rest/webservices/restfulwebservices/SpringSecurityBasicConfigurationAuth.java
@@ -0,0 +1,27 @@
+package com.alexandernissen.rest.webservices.restfulwebservices;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityBasicConfigurationAuth extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        ((HttpSecurity)((HttpSecurity)((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)http
+                .csrf().disable() // disabled in favor of JWT at a later stage
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll() // we permit all HTTP Options requests from any URL (/**)
+                                                                              // i.e., we allow pre-flight HTTP requests
+                .anyRequest()).authenticated()
+                .and())
+                //.formLogin().and()
+                )
+                .httpBasic(); // Use HTTP basic auth
+    }
+}
