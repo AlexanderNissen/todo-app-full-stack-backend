d92d6e18b3eeed295a77c4dc425cc55ad8acddaa
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-09-07 22:01:18 +0200

    S7S64 - Create Basic Auth RESTful service in Spring Boot

---
+++ src/main/java/com/alexandernissen/rest/webservices/restfulwebservices/AuthenticationBean.java
@@ -0,0 +1,22 @@
+package com.alexandernissen.rest.webservices.restfulwebservices;
+
+public class AuthenticationBean {
+    private String message;
+
+    public AuthenticationBean(String message) {
+        this.message = message;
+    }
+
+    public void setMessage() {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("AuthenticationBean [message=%s]", message);
+    }
+}
