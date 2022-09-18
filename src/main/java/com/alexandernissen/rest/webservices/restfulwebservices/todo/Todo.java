0692e092e7147a7906e08a0677874147f634abf9
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-09-18 22:54:30 +0200

    S9S74 - Define Todo as Entity and populate H2 in-mem DB

--- src/main/java/com/alexandernissen/rest/webservices/restfulwebservices/todo/Todo.java
+++ src/main/java/com/alexandernissen/rest/webservices/restfulwebservices/todo/Todo.java
@@ -1,10 +1,16 @@
 package com.alexandernissen.rest.webservices.restfulwebservices.todo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 public class Todo {
-  
-  private long id;
+
+  @Id
+  @GeneratedValue
+  private Long id;
   private String username;
   private String description;
   private Date targetDate;
@@ -14,7 +20,7 @@ public class Todo {
     // Empty constructor necessary to be able to update using put request
   }
   
-  public Todo(long id, String username, String description, Date targetDate, boolean isDone) {
+  public Todo(Long id, String username, String description, Date targetDate, boolean isDone) {
     super();
     this.id = id;
     this.username = username;
@@ -23,11 +29,11 @@ public class Todo {
     this.isDone = isDone;
   }
   
-  public long getId() {
+  public Long getId() {
     return this.id;
   }
 
-  public void setId(long id) {
+  public void setId(Long id) {
     this.id = id;
   }
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into todo(id, username, description, target_date, is_done)
+values(1001, 'Alexander', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(1002, 'Alexander', 'Learn Microservices', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(1003, 'Alexander', 'Learn Spring Data JPA', sysdate(), false);
+
